b1394fb72691a5ea189cd8a155bee9c4a247002a
projectilegravity <147197452+projectilegravity@users.noreply.github.com>
2026-01-11 20:15:39 +0100

    Update ProjectWorldToScreen.h

--- ProjectWorldToScreen.h
+++ ProjectWorldToScreen.h
@@ -1,26 +1,24 @@
 #pragma once
+#include <algorithm>
 
-inline fvector2d ProjectWorldToScreen(const fvector& world_pos)
+namespace math
 {
-    const auto& x_axis = g_Camera.transform_matrix.m[0];
-    const auto& y_axis = g_Camera.transform_matrix.m[1];
-    const auto& z_axis = g_Camera.transform_matrix.m[2];
+    inline fvector2d WorldToScreen(const fvector& world)
+    {
+        const auto& m = g_Camera.transform_matrix.m;
+        const fvector delta = world - g_Camera.cam_position;
 
-    const auto offset = world_pos - g_Camera.cam_position;
+        const float x = delta.dot({ m[1][0], m[1][1], m[1][2] });
+        const float y = delta.dot({ m[2][0], m[2][1], m[2][2] });
+        const float z = std::max(delta.dot({ m[0][0], m[0][1], m[0][2] }), 1.0f);
 
-    const auto proj_x = offset.dot(fvector(y_axis[0], y_axis[1], y_axis[2]));
-    const auto proj_y = offset.dot(fvector(z_axis[0], z_axis[1], z_axis[2]));
-    const auto proj_z = max(offset.dot(fvector(x_axis[0], x_axis[1], x_axis[2])), 1.0f);
+        const float scale = monitor.height_center / g_Camera.inverse_fov;
 
-    const float scale_factor = monitor.height_center / g_Camera.inverse_fov;
-    const auto screen_x = monitor.width_center + proj_x * scale_factor / proj_z;
-    const auto screen_y = monitor.height_center - proj_y * scale_factor / proj_z;
+        return { monitor.width_center  + (x * scale / z), monitor.height_center - (y * scale / z) };
+    }
 
-    return fvector2d(screen_x, screen_y);
-}
-
-inline bool IsPointOnScreen(const fvector2d& screen_pos)
-{
-    return (screen_pos.x > 0 && screen_pos.x < monitor.width &&
-            screen_pos.y > 0 && screen_pos.y < monitor.height);
+    inline bool OnScreen(const fvector2d& p)
+    {
+        return p.x > 0.f && p.x < monitor.width && p.y > 0.f && p.y < monitor.height;
+    }
 }
